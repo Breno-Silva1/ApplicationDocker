de6c35f90ad8111a0e780251f544e711b45bfd51
Breno-Silva1 <bbreno100@live.com>
2018-04-21 11:17:36 -0300

    Adicionando drive de conexão e alterações de propriedade

--- src/java/model/Dao.java
+++ src/java/model/Dao.java
@@ -5,23 +5,27 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class Dao {
+    private static Connection conexao;
     
+    public Dao() {
+        conexao = FabricaDeConexao.getConnection();
+    } 
     
     public ArrayList<Usuario> listarTodos() {
         ArrayList<Usuario> users = new ArrayList<>();
         try {
-                PreparedStatement preparedStatement = FabricaDeConexao.getConnection().prepareStatement("SELECT * FROM usuario;");
-                ResultSet resultSet = preparedStatement.executeQuery();
-                while (resultSet.next()) {
-                        
-                    String nome = resultSet.getString("nome");
-                    String email = resultSet.getString("email");
+            PreparedStatement preparedStatement = conexao.prepareStatement("SELECT * FROM usuario;");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
 
-                    Usuario u = new Usuario(nome, email);
-                    users.add(u);
-                }
+                String nome = resultSet.getString("nome");
+                String email = resultSet.getString("email");
+
+                Usuario u = new Usuario(nome, email);
+                users.add(u);
+            }
         } catch (SQLException e) {
-               System.out.println("Erro no banco de dados: ");
+            System.out.println("Erro no banco de dados: ");
         }
         return users;
     }
